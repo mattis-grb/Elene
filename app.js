a2806c5efeb1e64b2af4d5c0c53c7ac85bd15163
mattis-grb <mattis.boite.perso@gmail.com>
2026-06-24 00:33:22 +0200

    Update app.js

--- app.js
+++ app.js
@@ -52,30 +52,18 @@ document.querySelectorAll(".hidden")
 
 /* METTRE VOTRE DATE ICI */
 const startDate = new Date("2025-03-09");
+const freezeDate = new Date("2026-06-24");
 
-function updateCounter(){
+function updateCounter() {
+    const diff = freezeDate - startDate;
 
-    const now = new Date();
-    const diff = now - startDate;
-
-    const totalDays = Math.floor(diff / (1000 * 60 * 60 * 24));
-
-    const years = Math.floor(totalDays / 365);
-    const months = Math.floor((totalDays % 365) / 30);
-    const days = (totalDays % 365) % 30;
-
-    const hours = Math.floor(diff / 3600000) % 24;
-    const minutes = Math.floor(diff / 60000) % 60;
-    const seconds = Math.floor(diff / 1000) % 60;
+    const days = Math.floor(diff / (1000 * 60 * 60 * 24));
 
     document.getElementById("counter").innerHTML =
-    `${years} an(s)<br>
-    ${months} mois<br>
-    ${days} jour(s)<br>
-    ${hours}h ${minutes}m ${seconds}s`;
-
+        `${days} jours`;
 }
 
+updateCounter();
 setInterval(updateCounter, 1000);
 updateCounter();
 
